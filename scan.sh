3f72f454c3ab504eda298521f96fa22dd191d43b
coaxial <hi@64b.it>
2017-11-26 14:03:52 -0500

    Add photo mode to scan script

--- scan.sh
+++ scan.sh
@@ -2,48 +2,104 @@
 set -euo pipefail
 
 scripts_root="./etc/insaned/events"
+trimmer_location="$HOME/Downloads/trimmer"
 pages_scanned=0
-orientation=p
+orientation=p # p or l
+doc_type="ocr" # photo or ocr
+trimEnabled=1 # for photo only
 
 while true; do
+  prompt="d to change document type ($doc_type), l to toggle orientation ($orientation), s to scan, c to combine, o for single page, b for batch, q to quit "
 
-  read -r -p "l to toggle orientation ($orientation), s to scan, c to combine, o for single page, b for batch, q to quit " -s -n 1 input
+  if [[ $doc_type = "photo" ]]; then
+    prompt="d to change document type ($doc_type), t to toggle trimming ($trimEnabled), o to scan, q to quit "
+  fi
+
+  read -r -p "$prompt" -s -n 1 input
   echo "$input"
 
-  if [[ $input = "l" ]]; then
-    if [[ $orientation = "p" ]]; then
-      orientation=l
+  if [[ $input = "d" ]]; then
+    if [[ $doc_type = "ocr" ]]; then
+      doc_type="photo"
     else
-      orientation=p
+      doc_type="ocr"
+    fi
+  fi
+
+  if [[ $doc_type = "photo" ]]; then
+    if [[ $input = "t" ]]; then
+      trimEnabled=$((trimEnabled==0))
     fi
 
-    echo "switching to $orientation"
-  fi
-  if [[ $input = "s" ]]; then
-    echo "scanning page..."
-    PAGE_ORIENTATION="$orientation" "$scripts_root"/file
-    pages_scanned=$((pages_scanned +1))
-    echo "ok"
+    if [[ $input = "o" ]]; then
+      checktrim() {
+        outfile_size=$(ls -s /tmp/scan_trimmed.pnm | cut -f1 -d " ")
+        # the trim fails sometimes and we end up with an empty file (i.e. a 4KB
+        # file)
+        if [[ "$outfile_size" = 4 ]]; then
+          echo "trimming failed, will rescan..."
+          getphoto
+        fi
+      }
+      getphoto() {
+        scanimage \
+          -p \
+          --resolution 600 \
+          --mode color \
+          > /tmp/scan.pnm
+        echo ""
+        doTrim
+      }
+      doTrim() {
+        if [[ $trimEnabled ]]; then
+          "$trimmer_location" -s "east,south" -f 91 /tmp/scan.pnm /tmp/scan_trimmed.pnm
+          checktrim
+        fi
+      }
+
+      getphoto
+      convert /tmp/scan_trimmed.pnm "$HOME/Documents/scans/photo_$(date "+%Y%m%d_%H%M%S").png"
+      pages_scanned=$((pages_scanned +1))
+      echo "ok"
+    fi
   fi
 
-  if [[ $input = "c" ]]; then
-    echo "combining pages..."
-    "$scripts_root"/extra
-    echo "ok"
-  fi
+  if [[ $doc_type = "ocr" ]]; then
+    if [[ $input = "l" ]]; then
+      if [[ $orientation = "p" ]]; then
+        orientation=l
+      else
+        orientation=p
+      fi
 
-  if [[ $input = "o" ]]; then
-    echo "scanning one page document..."
-    PAGE_ORIENTATION="$orientation" "$scripts_root"/file
-    pages_scanned=$((pages_scanned +1))
-    "$scripts_root"/extra
-    echo "ok"
-  fi
+      echo "switching to $orientation"
+    fi
+    if [[ $input = "s" ]]; then
+      echo "scanning page..."
+      PAGE_ORIENTATION="$orientation" "$scripts_root"/file
+      pages_scanned=$((pages_scanned +1))
+      echo "ok"
+    fi
 
-  if [[ $input = "b" ]]; then
-    echo "batch mode scanning..."
-    BATCH_MODE=true "$scripts_root"/file
-    "$scripts_root"/extra
+    if [[ $input = "c" ]]; then
+      echo "combining pages..."
+      "$scripts_root"/extra
+      echo "ok"
+    fi
+
+    if [[ $input = "o" ]]; then
+      echo "scanning one page document..."
+      PAGE_ORIENTATION="$orientation" "$scripts_root"/file
+      pages_scanned=$((pages_scanned +1))
+      "$scripts_root"/extra
+      echo "ok"
+    fi
+
+    if [[ $input = "b" ]]; then
+      echo "batch mode scanning..."
+      BATCH_MODE=true "$scripts_root"/file
+      "$scripts_root"/extra
+    fi
   fi
 
   if [[ $input = "q" ]]; then
